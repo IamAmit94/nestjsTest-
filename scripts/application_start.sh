829f1701c2ece52176ed13e1471c0dede62b09d7
Amit Rawat <amitrvt18@hotmail.com>
2021-08-05 16:27:50 +0530

    changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,8 +11,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+nvm install 
 #install node modules
-npm install
+npm install 12.22.4
 npm run build
 node dist/src/main.js  
 #start our nestJs app in the background
