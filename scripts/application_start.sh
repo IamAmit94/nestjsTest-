08cf3b9cf0dcc0c09eb0d6ca011753a73eaf3f94
Amit Rawat <amitrvt18@hotmail.com>
2021-08-05 18:25:05 +0530

    changes made

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,10 +11,20 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+#install node
 nvm install 12.22.4
+
 #install node modules
 npm install 
+
+#install pm2
+npm install -g pm2
+
+#Nest app build
 npm run build
-pm2 start dist/src/main.js > app.out.log 2> app.err.log < /dev/null & 
+
+#pm2 clear all
+pm2 kill
+
 #start our nestJs app in the background
-#node app.js 
+pm2 start dist/src/main.js > app.out.log 2> app.err.log < /dev/null & 
